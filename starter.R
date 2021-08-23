e8b1779fc5ea5b0822b914a65745a992870dea38
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2021-08-23 12:36:20 +0200

    Initial commit

---
+++ starter.R
@@ -0,0 +1,30 @@
+# Starter
+
+getwd()
+
+setwd("/Users/nbr/Dropbox/HEP/2021-2022/BP - cours à option/2021_831_839_pdca")
+
+# Projet
+usethis::create_project("/Users/nbr/Dropbox/HEP/2021-2022/BP - cours à option/2021_831_839_pdca")
+
+# git
+
+usethis::use_git()
+
+usethis::use_github()
+
+# Step 1: Stage all modified files
+# git add .
+
+
+# Step 2: Describe the changes you made to your files
+# git commit -m "<A brief but descriptive commit message>"
+
+# Step 3: Push the changes to your GitHub repository
+# git push
+
+# Deploying 
+#usethis::use_github_pages(branch = "master")
+
+
+
